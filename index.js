15719fed21b189668d3d91758bca10104a54c95f
durbanitas <34290127+durbanitas@users.noreply.github.com>
2020-05-10 16:35:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ app.use((req, res, next) => {
 });
 
 app.get('/ratios/images?q=', (req, res) => {
-  console.log(req.q);
   request(
     { url: 'https://images-api.nasa.gov/search?q=' + 'xs' + '&media_type=image' },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
       }
-
+      console.log(req.q);
       res.json(JSON.parse(body));
     }
   )
